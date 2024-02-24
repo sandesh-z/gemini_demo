ae28d2751081f9d2c3bf8a96ea3de41acc97b0b5
sandesh <gsandace@gmail.com>
2024-02-24 21:38:15 +0545

    gemini demo project setup

---
+++ lib/home.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _inputController = TextEditingController();
+  bool _loading = false;
+  final ScrollController _scrollController = ScrollController();
+  final apiKey = "AIzaSyD5wrUI3oDFkF2QdrgHYaezUvnH9eA5VcY";
+  late ChatSession _session;
+  // final model = GenerativeModel(model:,apiKey: apiKey )
+
+  void _scrollDown() {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(
+          milliseconds: 750,
+        ),
+        curve: Curves.easeOutCirc,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        Expanded(
+          child: ListView(
+            controller: _scrollController,
+            padding: const EdgeInsets.all(16.0),
+            children: [],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _inputController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your prompt',
+                  ),
+                  onEditingComplete: () {
+                    if (!_loading) {
+                      _sendMessage();
+                    }
+                  },
+                ),
+              ),
+              _loading
+                  ? const CircularProgressIndicator()
+                  : IconButton(
+                      onPressed: _sendMessage,
+                      icon: const Icon(Icons.send),
+                    ),
+            ],
+          ),
+        )
+      ]),
+    );
+  }
+
+  void _sendMessage() {
+    debugPrint(_inputController.text);
+  }
+}
